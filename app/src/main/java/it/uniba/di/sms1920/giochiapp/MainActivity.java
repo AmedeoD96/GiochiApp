3a6aaca985577002e719ed326e633926b342ea30
Luca ivone <luca.ivone@outlook.it>
2020-01-03 19:37:04 +0100

    Created android project

---
+++ app/src/main/java/it/uniba/di/sms1920/giochiapp/MainActivity.java
@@ -0,0 +1,13 @@
+package it.uniba.di.sms1920.giochiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
