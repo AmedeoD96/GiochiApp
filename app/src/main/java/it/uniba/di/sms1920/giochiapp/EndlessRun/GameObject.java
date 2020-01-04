45c813c26e53a8df333dbc5ea358ebd3ccb94f7f
lindro <stormo1398@gmail.com>
2020-01-04 11:53:26 +0100

    Fine video 2: Memento cubi

---
+++ app/src/main/java/it/uniba/di/sms1920/giochiapp/EndlessRun/GameObject.java
@@ -0,0 +1,9 @@
+package it.uniba.di.sms1920.giochiapp.EndlessRun;
+
+import android.graphics.Canvas;
+
+public interface GameObject {
+    public void draw(Canvas canvas);
+    public void update();
+
+}
---
+++ app/src/main/java/it/uniba/di/sms1920/giochiapp/EndlessRun/Obstacle.java
@@ -0,0 +1,39 @@
+package it.uniba.di.sms1920.giochiapp.EndlessRun;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+
+public class Obstacle implements GameObject {
+
+    private Rect rectangle;
+    private int color;
+
+    public Obstacle(Rect rectangle, int color) {
+        this.rectangle = rectangle;
+        this.color = color;
+    }
+
+    public boolean playerCollide(RectPlayer player) {
+        if(rectangle.contains(player.getRectangle().left, player.getRectangle().top)
+            || (rectangle.contains(player.getRectangle().right, player.getRectangle().top))
+            || (rectangle.contains(player.getRectangle().left, player.getRectangle().bottom))
+            || (rectangle.contains(player.getRectangle().right, player.getRectangle().bottom)) ) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(rectangle,paint);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
