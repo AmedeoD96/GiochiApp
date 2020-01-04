6ad9e5e27e53ce748be62c050edc67c2e85967bd
lindro <stormo1398@gmail.com>
2020-01-04 10:44:46 +0100

    endless run begins (come batman)

---
+++ app/src/main/java/it/uniba/di/sms1920/giochiapp/EndlessRun/GamePanel.java
@@ -0,0 +1,4 @@
+package it.uniba.di.sms1920.giochiapp.EndlessRun;
+
+public class GamePanel {
+}
---
+++ app/src/main/java/it/uniba/di/sms1920/giochiapp/EndlessRun/MainActivityrun.java
@@ -0,0 +1,14 @@
+package it.uniba.di.sms1920.giochiapp.EndlessRun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityrun extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainrun);
+    }
+}
