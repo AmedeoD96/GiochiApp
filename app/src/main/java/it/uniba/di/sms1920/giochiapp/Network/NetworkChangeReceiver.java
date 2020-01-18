5c7850094fb1abd53a7396ad7b9da8afe92790b9
lindro <stormo1398@gmail.com>
2020-01-18 18:05:12 +0100

    Anche il package Network sta commentato (Luca controlla)

--- app/src/main/java/it/uniba/di/sms1920/giochiapp/Network/NetworkChangeReceiver.java
+++ app/src/main/java/it/uniba/di/sms1920/giochiapp/Network/NetworkChangeReceiver.java
@@ -10,11 +10,12 @@ import java.util.List;
 
 public class NetworkChangeReceiver extends BroadcastReceiver {
 
-    private final String CONNECTIVITY_CHANGE = "android.net.conn.CONNECTIVITY_CHANGE";
+    //Lista contenente le Callback 
     List<INetworkCallback> networkCallbacks = new ArrayList<>();
 
     private static NetworkChangeReceiver _instance;
 
+    //assegna la classe
     public NetworkChangeReceiver() {
         super();
         _instance = this;
@@ -25,10 +26,12 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
     }
 
 
+    //aggiunge una callback
     public void registerCallback(INetworkCallback callback) {
         networkCallbacks.add(callback);
     }
 
+    //mai usato
     public boolean removeCallback(INetworkCallback callback) {
         return networkCallbacks.remove(callback);
     }
@@ -37,14 +40,19 @@ public class NetworkChangeReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent intent) {
 
+        //si ottiene il tipo di connessione
         final int status = NetworkUtil.getConnectivityStatusString(context);
         //Log.i("NetworkDebug", "Connection status: " + status);
 
+        String CONNECTIVITY_CHANGE = "android.net.conn.CONNECTIVITY_CHANGE";
         if (CONNECTIVITY_CHANGE.equals(intent.getAction())) {
+            //se il tipo di connessione dovesse cambiare in corso d'opera, allora si assegna allo stato della connessione
+            //la variabile che indica il "non connesso"
             boolean isNetworkPresent = status == NetworkUtil.NETWORK_STATUS_NOT_CONNECTED;
 
             //DatabaseManager.getInstance().setUseLocalVsRemote(isNetworkPresent);
 
+            //in caso di cambiamenti della connessione verrebbero aggiornate tutte le callback
             for (INetworkCallback networkCallback : networkCallbacks) {
                 networkCallback.OnNetworkChange(isNetworkPresent);
             }
